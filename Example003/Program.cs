cf045195b7510f0399fcba73e13553bc8e7ba827
Alexey_Volkov <alexiy87@inbox.ru>
2023-02-09 03:43:17 +0300

    feat:add a new task

---
+++ Example003/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        System.Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0 && result < 8)
+        {
+            break;
+        }
+        else System.Console.WriteLine("Не правильный ввод, повторите");
+    }
+    return result;
+}
+
+int number = GetNumber("Введите цифровое значения дня недели");
+
+if (number > 5)
+{
+    System.Console.WriteLine("Сегодня выходной");
+
+}
+else System.Console.WriteLine("Сегодня будний день");
